ed4fb6202f75b79e9c42d60f39bf47890edc5a2d
Nathan Pahucki <npahucki@gmail.com>
2014-04-10 08:13:15 -0300

    Tweak on Details screen.

--- ios/Milestones/Milestones/MilestoneDetailsViewController.m
+++ ios/Milestones/Milestones/MilestoneDetailsViewController.m
@@ -25,6 +25,19 @@
   self.ageRangeLabel.text = [NSString stringWithFormat:@"%@ - %@ days", self.achievement.standardMilestone.rangeLow, self.achievement.standardMilestone.rangeHigh];
 }
 
+-(void) viewDidAppear:(BOOL)animated {
+  // TODO: Fix jumpiness caused by resizing to fit.
+  [super viewDidAppear:animated];
+  [self.titleLabel sizeToFit];
+  [self.descriptionLabel sizeToFit];
+}
+
+//-(void) viewDidLayoutSubviews {
+//  [super viewDidLayoutSubviews];
+//  [self.titleLabel sizeToFit];
+//  [self.descriptionLabel sizeToFit];
+//}
+
 -(void) prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     ((NoteMilestoneViewController*)segue.destinationViewController).achievement = self.achievement;
 }
