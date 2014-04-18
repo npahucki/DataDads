3b3268e9140417c937a548332565072dfc904a25
Nathan Pahucki <npahucki@gmail.com>
2014-04-18 18:43:52 -0300

    Make signup screen comply with look and feel

---
+++ ios/Milestones/Milestones/SignUpViewController.h
@@ -0,0 +1,13 @@
+//
+//  SignupViewController.h
+//  Milestones
+//
+//  Created by Nathan  Pahucki on 4/18/14.
+//  Copyright (c) 2014 Nathan  Pahucki. All rights reserved.
+//
+
+#import <Parse/Parse.h>
+
+@interface SignupViewController : PFSignUpViewController
+
+@end
