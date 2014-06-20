5d2d26b7bc4d0b29deebae2b760da81fc92a14aa
Nathan Pahucki <npahucki@gmail.com>
2014-06-20 17:03:12 -0400

    Add a way to test CLoudCode locally!!!!

---
+++ parse-cloud-code/tests/local_testing.js
@@ -0,0 +1,26 @@
+(function () {
+      global.Parse = require("parse-cloud-debugger").Parse;
+      global.
+      Parse.initialize( "NlJHBG0NZgFS8JP76DBjA31MBRZ7kmb7dVSQQz3U", "Km9C7vBKrLdnDf8Uc3Zgf3qdw3qmbYa13R8RD1q2", "ScHR4mshg3TICZKsbPiLmCFLEidiChAwLpWHIUCO");
+})
+();
+
+var runOnParse = false;
+var originalParseFunction = Parse._request;
+
+Parse._request = function (options) {
+    Parse.serverURL = "https://api.parse.com";
+
+    if (runOnParse === false && options.route == "functions") {
+        Parse.serverURL = "http://localhost:5555";
+    }
+
+    return originalParseFunction(options);
+};
+
+var originalRequire = require;
+require = function(lib) {
+    return originalRequire(lib);
+};
+
+require("../cloud/main.js");
