cb3d3932d389383eeff32399e883d6ed946203c5
Nathan Pahucki <npahucki@gmail.com>
2014-07-04 11:38:59 -0400

    Remove Tags Screen.

--- ios/Milestones/BannerViewController.m
+++ ios/Milestones/BannerViewController.m
@@ -1,7 +1,7 @@
 /*
  File: BannerViewController.m
- Abstract: A container view controller that manages an ADBannerView and a content view controller
- Version: 2.2
+ Abstract: A container view controller that manages an ADBannerView and a content view controller.
+ Version: 1.2
  
  Disclaimer: IMPORTANT:  This Apple software is supplied to you by Apple
  Inc. ("Apple") in consideration of your agreement to the following
@@ -41,21 +41,16 @@
  STRICT LIABILITY OR OTHERWISE, EVEN IF APPLE HAS BEEN ADVISED OF THE
  POSSIBILITY OF SUCH DAMAGE.
  
- Copyright (C) 2013 Apple Inc. All Rights Reserved.
+ Copyright (C) 2014 Apple Inc. All Rights Reserved.
  
  */
 
 #import "BannerViewController.h"
+#import <iAd/iAd.h>
 
 NSString * const BannerViewActionWillBegin = @"BannerViewActionWillBegin";
 NSString * const BannerViewActionDidFinish = @"BannerViewActionDidFinish";
 
-@interface BannerViewController ()
-
-// This method is used by BannerViewSingletonController to inform instances of BannerViewController that the banner has loaded/unloaded.
-- (void)updateLayout;
-
-@end
 
 @interface BannerViewManager : NSObject <ADBannerViewDelegate>
 
@@ -68,91 +63,76 @@ NSString * const BannerViewActionDidFinish = @"BannerViewActionDidFinish";
 
 @end
 
-@implementation BannerViewController {
-  UIViewController *_contentController;
-}
 
-- (instancetype)initWithContentViewController:(UIViewController *)contentController
-{
-  // If contentController is nil, -loadView is going to throw an exception when it attempts to setup
-  // containment of a nil view controller.  Instead, throw the exception here and make it obvious
-  // what is wrong.
-  NSAssert(contentController != nil, @"Attempting to initialize a BannerViewController with a nil contentController.");
-  
-  self = [super init];
-  if (self != nil) {
-    _contentController = contentController;
-    self.tabBarItem = _contentController.tabBarItem;
-    //[self setEdgesForExtendedLayout:UIRectEdgeNone];
-    //self.extendedLayoutIncludesOpaqueBars=NO;
-    //self.automaticallyAdjustsScrollViewInsets=NO;
-    [[BannerViewManager sharedInstance] addBannerViewController:self];
-  }
-  return self;
-}
+#pragma mark -
+
+@interface BannerViewController ()
+@property (nonatomic, strong) UIViewController *contentController;
+@end
+
+
+#pragma mark -
+
+@implementation BannerViewController
 
 - (void)dealloc
 {
   [[BannerViewManager sharedInstance] removeBannerViewController:self];
 }
 
-- (void)loadView
+- (void)viewDidLoad
 {
-  UIView *contentView = [[UIView alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-  // Setup containment of the _contentController.
-  [self addChildViewController:_contentController];
-  [contentView addSubview:_contentController.view];
-  [_contentController didMoveToParentViewController:self];
+  [super viewDidLoad];
   
-  self.view = contentView;
+  [[BannerViewManager sharedInstance] addBannerViewController:self];
+  
+  NSArray *children = self.childViewControllers;
+  assert(children != nil);    // must have children
+  
+  // keep track of our child view controller for rotation and frame management
+  _contentController = children[0];
 }
 
 - (UIInterfaceOrientation)preferredInterfaceOrientationForPresentation
 {
-  return [_contentController preferredInterfaceOrientationForPresentation];
+  return [self.contentController preferredInterfaceOrientationForPresentation];
 }
 
 - (NSUInteger)supportedInterfaceOrientations
 {
-  return [_contentController supportedInterfaceOrientations];
+  return [self.contentController supportedInterfaceOrientations];
 }
 
 - (void)viewDidLayoutSubviews
 {
+  [super viewDidLayoutSubviews];
+  
   CGRect contentFrame = self.view.bounds, bannerFrame = CGRectZero;
   ADBannerView *bannerView = [BannerViewManager sharedInstance].bannerView;
-  // If configured to support iOS >= 6.0 only, then we want to avoid currentContentSizeIdentifier as it is deprecated.
-  // Fortunately all we need to do is ask the banner for a size that fits into the layout area we are using.
-  // At this point in this method contentFrame=self.view.bounds, so we'll use that size for the layout.
   bannerFrame.size = [bannerView sizeThatFits:contentFrame.size];
-  
   if (bannerView.bannerLoaded) {
     contentFrame.size.height -= bannerFrame.size.height;
-    bannerFrame.origin.y = contentFrame.size.height - self.tabBarController.tabBar.bounds.size.height;
+    bannerFrame.origin.y = contentFrame.size.height;
   } else {
     bannerFrame.origin.y = contentFrame.size.height;
   }
-  _contentController.view.frame = contentFrame;
-  // We only want to modify the banner view itself if this view controller is actually visible to the user.
-  // This prevents us from modifying it while it is being displayed elsewhere.
+  self.contentController.view.frame = contentFrame;
+  
+  // We only want to modify the banner view itself if this view controller is actually
+  // visible to the user. This prevents us from modifying it while it is being displayed elsewhere.
+  //
   if (self.isViewLoaded && (self.view.window != nil)) {
     [self.view addSubview:bannerView];
     bannerView.frame = bannerFrame;
+    [self.view layoutSubviews]; // required by auto layout
   }
 }
 
 - (void)updateLayout
 {
-  [UIView animateWithDuration:0.5 animations:^{
-    // -viewDidLayoutSubviews will handle positioning the banner such that it is either visible
-    // or hidden depending upon whether its bannerLoaded property is YES or NO.  We just need our view
-    // to (re)lay itself out so -viewDidLayoutSubviews will be called.
-    // You must not call [self.view layoutSubviews] directly.  However, you can flag the view
-    // as requiring layout...
+  [UIView animateWithDuration:0.25 animations:^{
     [self.view setNeedsLayout];
-    // ...then ask it to lay itself out immediately if it is flagged as requiring layout...
     [self.view layoutIfNeeded];
-    // ...which has the same effect.
   }];
 }
 
@@ -164,10 +144,13 @@ NSString * const BannerViewActionDidFinish = @"BannerViewActionDidFinish";
 
 - (NSString *)title
 {
-  return _contentController.title;
+  return self.contentController.title;
 }
 
 @end
+
+
+#pragma mark -
 
 @implementation BannerViewManager {
   ADBannerView *_bannerView;
@@ -188,7 +171,12 @@ NSString * const BannerViewActionDidFinish = @"BannerViewActionDidFinish";
 {
   self = [super init];
   if (self != nil) {
-    _bannerView = [[ADBannerView alloc] initWithAdType:ADAdTypeBanner];
+    // On iOS 6 ADBannerView introduces a new initializer, use it when available.
+    if ([ADBannerView instancesRespondToSelector:@selector(initWithAdType:)]) {
+      _bannerView = [[ADBannerView alloc] initWithAdType:ADAdTypeBanner];
+    } else {
+      _bannerView = [[ADBannerView alloc] init];
+    }
     _bannerView.delegate = self;
     _bannerViewControllers = [[NSMutableSet alloc] init];
   }
@@ -207,6 +195,8 @@ NSString * const BannerViewActionDidFinish = @"BannerViewActionDidFinish";
 
 - (void)bannerViewDidLoadAd:(ADBannerView *)banner
 {
+  NSLog(@"bannerViewDidLoadAd");
+  
   for (BannerViewController *bvc in _bannerViewControllers) {
     [bvc updateLayout];
   }
@@ -214,6 +204,8 @@ NSString * const BannerViewActionDidFinish = @"BannerViewActionDidFinish";
 
 - (void)bannerView:(ADBannerView *)banner didFailToReceiveAdWithError:(NSError *)error
 {
+  NSLog(@"didFailToReceiveAdWithError %@", error);
+  
   for (BannerViewController *bvc in _bannerViewControllers) {
     [bvc updateLayout];
   }
@@ -229,6 +221,5 @@ NSString * const BannerViewActionDidFinish = @"BannerViewActionDidFinish";
 {
   [[NSNotificationCenter defaultCenter] postNotificationName:BannerViewActionDidFinish object:self];
 }
-
 
 @end
