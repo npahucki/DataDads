1ff6149ca9b8624ad3516310267fdf20a59875a5
Nathan Pahucki <npahucki@gmail.com>
2014-07-25 14:57:15 -0400

    Hide status bar on intro screen.

--- ios/Milestones/IntroScreenPageViewController.m
+++ ios/Milestones/IntroScreenPageViewController.m
@@ -21,6 +21,18 @@
     self.continueButton.titleLabel.font = [UIFont fontForAppWithType:Book andSize:21];
     [self.continueButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
     [self.loginNowButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [[UIApplication sharedApplication] setStatusBarHidden:YES];
+}
+
+-(void)viewWillDisappear:(BOOL)animated{
+    [super viewWillDisappear:animated];
+    [[UIApplication sharedApplication] setStatusBarHidden:NO withAnimation:UIStatusBarAnimationFade];
+}
+
+
+-(void)viewDidAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    [[UIApplication sharedApplication] setStatusBarHidden:YES withAnimation:UIStatusBarAnimationFade];
 }
 
 @end
