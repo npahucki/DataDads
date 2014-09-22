d3b44823f7c3ebcb4e9cce501e7be62c6d8eac20
Nathan Pahucki <npahucki@gmail.com>
2014-09-22 17:20:17 -0300

    Make video rotate automatically in browser.

--- parse-cloud-code/cloud/controllers/achievements.js
+++ parse-cloud-code/cloud/controllers/achievements.js
@@ -12,41 +12,68 @@ exports.show = function (req, res) {
     query.get(achievementId).then(function (achievement) {
                 // TODO: Temporary work around to bug in client where sharedVia is not updated when shared via email
                 var isShared = true; //achievement.get("sharedVia") > 0;
-                if(isShared) {
+                if (isShared) {
                     var title = achievement.get("customTitle");
                     var milestone = achievement.get("standardMilestone");
                     var baby = achievement.get("baby");
-                    if(!title) title = milestone.get("title");
-                    title = utils.replacePronounTokens(title,baby.get("isMale"), "en");
+                    if (!title) title = milestone.get("title");
+                    title = utils.replacePronounTokens(title, baby.get("isMale"), "en");
                     var hasImage = achievement.get("attachmentType") && achievement.get("attachmentType").indexOf("image/") == 0;
                     var hasVideo = achievement.get("attachmentType") && achievement.get("attachmentType").indexOf("video/") == 0;
 
-                    if(hasImage) {
+                    if (hasImage) {
                         res.render('achievements/show_photo', {
-                            title: title,
-                            completedOn : achievement.get("completedOn"),
-                            photoUrl : achievement.get("attachment").url()
+                            title:title,
+                            completedOn:achievement.get("completedOn"),
+                            photoUrl:achievement.get("attachment").url()
                         });
-                    } else if(hasVideo) {
+                    } else if (hasVideo) {
+                        var width = achievement.get("attachmentWidth");
+                        var height = achievement.get("attachmentHeight");
+                        var orientation = achievement.get("attachmentOrientation");
+                        var rotation = 0;
+                        switch (orientation) {
+                            case 1: // Down.
+                                rotation = 180;
+                                break;
+                            case 2: // Left
+                                // Swap height and width
+                                height = achievement.get("attachmentWidth");
+                                width = achievement.get("attachmentHeight");
+                                rotation = 270;
+                                break;
+                            case 3: // Right
+                                // Swap height and width
+                                height = achievement.get("attachmentWidth");
+                                width = achievement.get("attachmentHeight");
+                                rotation = 90;
+                                break;
+                        }
+
+                        console.log("ORIENTATION:" + orientation +  " WIDTH: " + width + " HEIGHT:" + height + " ROTATE:" + rotation);
+
                         res.render('achievements/show_video', {
-                            title: title,
-                            completedOn : achievement.get("completedOn"),
-                            thumbnailUrl : achievement.get("attachmentThumbnail").url(),
-                            videoUrl : achievement.get("attachment").url()
+                            title:title,
+                            completedOn:achievement.get("completedOn"),
+                            thumbnailUrl:achievement.get("attachmentThumbnail").url(),
+                            videoUrl:achievement.get("attachment").url(),
+                            videoWidth:width,
+                            videoHeight:height,
+                            videoRotation:rotation
                         });
                     } else {
                         res.render('achievements/show_no_media', {
-                            title: title,
-                            completedOn : achievement.get("completedOn")
+                            title:title,
+                            completedOn:achievement.get("completedOn")
                         });
                     }
 
                 } else {
-                    res.send(404,"Achievement is not shared");
+                    res.send(404, "Achievement is not shared");
                 }
             },
             function (error) {
                 console.error(JSON.stringify(error));
-                res.send(500,"Could not display Achievement");
+                res.send(500, "Could not display Achievement");
             });
 };
