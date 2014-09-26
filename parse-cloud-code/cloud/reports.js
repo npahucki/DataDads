13d2c082ead86e0b8a70833b690ab6f1df019524
Nathan Pahucki <npahucki@gmail.com>
2014-09-26 17:34:00 -0300

    Firt working daily summary report.

--- parse-cloud-code/cloud/reports.js
+++ parse-cloud-code/cloud/reports.js
@@ -1,4 +1,6 @@
 Parse.Cloud.job("generateSummaryReport", function (request, status) {
+
+
     console.log("Starting Summary Report Generation Job...");
     Parse.Cloud.useMasterKey();
     var _ = require("underscore");
@@ -6,88 +8,167 @@ Parse.Cloud.job("generateSummaryReport", function (request, status) {
     var util = require("cloud/utils");
     var lastWeek = util.dateAddDays(new Date(), -7);
 
+    function coalateStats(statsQuery, objectQuery) {
+        return statsQuery.find()
+                .then(function (statsResults) {
+                    var counts = _.reduce(statsResults, function (map, stat) {
+                        map[stat.get("refObjectId")] = stat.get("count");
+                        return map;
+                    }, {});
+                    objectQuery.containedIn("objectId", _.keys(counts));
+                    return Parse.Promise.when(counts, objectQuery.find());
+                }).then(function (counts, objects) {
+                    var stats = _.map(objects, function (object) { return { object:object, count:counts[object.id]}});
+                    return Parse.Promise.as(_.sortBy(stats, function (stat) { return -(stat.count) }));
+                });
+    }
+
+    function milestoneStatsToText(statsQuery) {
+        var milestoneQuery = new Parse.Query("StandardMilestones");
+        return coalateStats(statsQuery, milestoneQuery).then(function(stats) {
+            var text = "<ol>";
+            _.each(stats, function (stat) {
+                var milestone = stat.object;
+                if (milestone) { // null if deleted for some reason
+                    text += "<li>" + milestone.get("title") + "(" + stat.count + ")</li>";
+                }
+            });
+            text += "</ol>";
+            return Parse.Promise.as(text);
+        });
+    }
+
+
     // Number of new users in last 7 days.
     var newUserQuery = new Parse.Query(Parse.User);
     newUserQuery.greaterThan("createdAt", lastWeek);
     promises.push(newUserQuery.count());
+
+    // Number of new babies in last 7 days.
+    var newBabiesQuery = new Parse.Query("Babies");
+    newBabiesQuery.greaterThan("createdAt", lastWeek);
+    promises.push(newBabiesQuery.count());
+
+    // Number of new installs in last 7 days.
+    var newInstallQuery = new Parse.Query(Parse.Installation);
+    newInstallQuery.greaterThan("createdAt", lastWeek);
+    promises.push(newInstallQuery.count());
+
+
+
+
+    var anonUserQuery = new Parse.Query(Parse.User);
+    anonUserQuery.doesNotExist("email");
+    promises.push(anonUserQuery.count());
+
+    var signedInUserQuery = new Parse.Query(Parse.User);
+    signedInUserQuery.exists("email");
+    promises.push(signedInUserQuery.count());
+
+
 
     // Number of new milestones in last 7 days
     var newMilestoneQuery = new Parse.Query("MilestoneAchievements");
     newMilestoneQuery.greaterThan("createdAt", lastWeek);
     promises.push(newMilestoneQuery.count());
 
+    // Total logged milestones
+    var allMilestoneQuery = new Parse.Query("MilestoneAchievements");
+    promises.push(allMilestoneQuery.count());
+
+
     // 20 Most active users
     var mostActiveBabiesStatQuery = new Parse.Query("Stats");
     mostActiveBabiesStatQuery.equalTo("type", "babyNotedMilestoneCount");
     mostActiveBabiesStatQuery.limit(20);
     mostActiveBabiesStatQuery.addDescending("count");
-    promises.push(mostActiveBabiesStatQuery.find()
-            .then(function (statsResults) {
-                var counts = _.reduce(statsResults, function (map, stat) {
-                    map[stat.get("refObjectId")] = stat.get("count");
-                    return map;
-                }, {});
-                var mostActiveBabiesQuery = new Parse.Query("Babies");
-                mostActiveBabiesQuery.containedIn("objectId", _.keys(counts));
-                mostActiveBabiesQuery.include("parentUser");
-                return mostActiveBabiesQuery.find().then(function (babies) {
-                    var stats = _.map(babies, function (baby) { return { baby:baby, count:counts[baby.id]}});
-                    stats = _.sortBy(stats, function (stat) { return -(stat.count) });
-                    var mostActiveUserText = "<ol>";
-                    _.each(stats, function (stat) {
-                        // TODO: This will need adjustment when you can track two babies
-                        var parent = stat.baby.get("parentUser");
-                        if (parent) {
-                            mostActiveUserText += "<li>" + (parent.get("email") ? parent.get("email") : parent.id) + "&lt;" + parent.get("screenName") + "&gt; (" + stat.count + ")</li>";
-                        }
-                    });
-                    mostActiveUserText += "</ol>";
-                    return Parse.Promise.as(mostActiveUserText);
-                });
-            })
-    );
+    mostActiveBabiesStatQuery.greaterThan("updatedAt", lastWeek);
+    var babyQuery = new Parse.Query("Babies");
+    babyQuery.include("parentUser");
+    promises.push(coalateStats(mostActiveBabiesStatQuery, babyQuery).then(function(stats) {
+        var mostActiveUserText = "<ol>";
+        _.each(stats, function (stat) {
+            // TODO: This will need adjustment when you can track two babies
+            var baby = stat.object;
+            var parent = baby.get("parentUser");
+            if (parent) {
+                mostActiveUserText += "<li>" + (parent.get("email") ? parent.get("email") : parent.id) + "&lt;" + parent.get("screenName") + "&gt; (" + stat.count + ")</li>";
+            }
+        });
+        mostActiveUserText += "</ol>";
+        return Parse.Promise.as(mostActiveUserText);
+    }));
 
+    // 20 Most commonly logged milestones (7 days)
+    var mostActiveMilestonesThisWeekQuery = new Parse.Query("Stats");
+    mostActiveMilestonesThisWeekQuery.equalTo("type", "standardMilestoneNotedCount");
+    mostActiveMilestonesThisWeekQuery.limit(20);
+    mostActiveMilestonesThisWeekQuery.addDescending("count");
+    mostActiveMilestonesThisWeekQuery.greaterThan("updatedAt", lastWeek);
+    promises.push(milestoneStatsToText(mostActiveMilestonesThisWeekQuery));
 
-//    var statsToText = function (stats) {
-//        var text = "<ol>";
-//        _.each(stats, function (stat) {
-//            var milestone = stat.get("standardMilestone");
-//            if (milestone) { // null if deleted for some reason
-//                text += "<li>" + milestone.get("title") + "(" + stat.get("count") + ")" < /li>";
-//            }
-//        });
-//        text += "</ol>";
-//        return Parse.Promise.as(text);
-//    };
-//
-//    // 20 Most commonly logged milestones (7 days)
-//    var mostActiveMilestonesThisWeekQuery = new Parse.Query("StandardMilestoneStats");
-//    mostActiveMilestonesThisWeekQuery.include(["standardMilestone"]);
-//    mostActiveMilestonesThisWeekQuery.limit(20);
-//    mostActiveMilestonesThisWeekQuery.addDescending("count");
-//    newMilestoneQuery.greaterThan("updatedAt", lastWeek);
-//    promises.push(mostActiveMilestonesThisWeekQuery.find().then(statsToText));
-//
-//// 20 Most common milestones (all time)
-//    var mostActiveMilestonesQuery = new Parse.Query("StandardMilestoneStats");
-//    mostActiveMilestonesQuery.include(["standardMilestone"]);
-//    mostActiveMilestonesQuery.limit(20);
-//    mostActiveMilestonesQuery.addDescending("count");
-//    promises.push(mostActiveMilestonesQuery.find().then(statsToText));
+    // 20 Most common milestones (all time)
+    var mostActiveMilestonesQuery = new Parse.Query("Stats");
+    mostActiveMilestonesQuery.equalTo("type", "standardMilestoneNotedCount");
+    mostActiveMilestonesQuery.limit(20);
+    mostActiveMilestonesQuery.addDescending("count");
+    promises.push(milestoneStatsToText(mostActiveMilestonesQuery));
 
-    Parse.Promise.when(promises).then(function (newUserCount, newMilestoneCount, mostActiveUsers, mostActiveMilestonesThisWeek, mostActiveMilestones) {
+    // 20 Most commonly skipped milestones (all time)
+    var mostSkippedMilestonesQuery = new Parse.Query("Stats");
+    mostSkippedMilestonesQuery.equalTo("type", "standardMilestoneSkippedCount");
+    mostSkippedMilestonesQuery.limit(20);
+    mostSkippedMilestonesQuery.addDescending("count");
+    promises.push(milestoneStatsToText(mostSkippedMilestonesQuery));
+
+    // 20 Most commonly postponed milestones (all time)
+    var mostPostponedMilestonesQuery = new Parse.Query("Stats");
+    mostPostponedMilestonesQuery.equalTo("type", "standardMilestonePostponedCount");
+    mostPostponedMilestonesQuery.limit(20);
+    mostPostponedMilestonesQuery.addDescending("count");
+    promises.push(milestoneStatsToText(mostPostponedMilestonesQuery));
+
+    Parse.Promise.when(promises).then(function (
+            newUserCount,
+            newBabiesCount,
+            newInstallCount,
+            anonUserCount,
+            signedInUserCount,
+            newMilestoneCount,
+            allMilestoneCount,
+            mostActiveUsers,
+            mostActiveMilestonesThisWeek,
+            mostActiveMilestones,
+            mostSkippedMilestones,
+            mostPostponedMilestones) {
 
         var reportText = "<html><body><h1>Report for " + new Date() + "</h1><ul>" +
-                "<li>New Users in the Last 7 days : " + newUserCount + "</li>" +
-                "<li>New Milestones in the last 7 days: " + newMilestoneCount + "</li>" +
-                "<li>Top 20 active users : " + mostActiveUsers + "</li>" +
-                "<li>Top 20 milestones in the last 7 days : " + mostActiveMilestonesThisWeek + "</li>" +
-                "<li>Top 20 milestones of all time : " + mostActiveMilestones + "</li>" +
+                "<li><b>New installs in the Last 7 days :</b> " + newInstallCount + "</li>" +
+                "<li><b>New users in the Last 7 days :</b> " + newUserCount + "</li>" +
+                "<li><b>New babies in the Last 7 days :</b> " + newBabiesCount + "</li>" +
+                "<li><b>New milestones in the last 7 days:</b> " + newMilestoneCount + "</li>" +
+                "<hr/>" +
+                "<li><b>Installs resulting in new babies in the last 7 days:</b> " + ((newBabiesCount / newInstallCount) * 100).toFixed(2) + "%</li>" +
+                "<hr/>" +
+                "<li><b>Total anonymous users :</b> " + anonUserCount + "</li>" +
+                "<li><b>Total signed in users :</b> " + signedInUserCount + "</li>" +
+                "<li><b>Total users :</b> " + (anonUserCount + signedInUserCount) + "</li>" +
+                "<li><b>Total number of logged milestones:</b> " + allMilestoneCount + "</li>" +
+                "<hr/>" +
+                "<li><b>Top 20 active users in the last 7 days :</b> " + mostActiveUsers + "</li>" +
+                "<hr/>" +
+                "<li><b>Top 20 milestones in the last 7 days :</b> " + mostActiveMilestonesThisWeek + "</li>" +
+                "</hr>" +
+                "<li><b>Top 20 milestones of all time :</b> " + mostActiveMilestones + "</li>" +
+                "<hr/>" +
+                "<li><b>Top 20 skipped milestones :</b> " + mostSkippedMilestones + "</li>" +
+                "<hr/>" +
+                "<li><b>Top 20 postponed of all time :</b> " + mostPostponedMilestones + "</li>" +
                 "</ul></body></html>";
 
         // Set the job's success status
         var emailer = require("cloud/teamnotify");
-        emailer.notify("Daily Summary Report", reportText);
+        emailer.notify("Daily DataParenting Activity", reportText, "text/html");
         status.success("Daily Summary Report completed successfully.");
     }, function (error) {
         // Set the job's error status
