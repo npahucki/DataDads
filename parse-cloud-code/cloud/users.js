b24d9a1304d34874335a856e5595182431652d29
Nathan Pahucki <npahucki@gmail.com>
2014-11-10 16:02:08 -0300

    Move risky stuff to afterSave to reduce risk of user not being saved.

--- parse-cloud-code/cloud/users.js
+++ parse-cloud-code/cloud/users.js
@@ -1,66 +1,59 @@
 Parse.Cloud.beforeSave(Parse.User, function (request, response) {
     var userObject = request.object;
     // Need before save beacuse after save does indicate what the dirty fields are.
-    if (userObject.get("email") && userObject.dirty("email")) {
-        // Wait until assignment is done.
-        userObject.set("needsTipAssignmentNow", true);
-        
-        var userName = userObject.get('email');
-        var subject = "A user has signed up: " + userName;
-
-        var notificationObject = {};
-        notificationObject.user = userObject;
-
-        var babyQuery = new Parse.Query("Babies");
-            
-        babyQuery.equalTo("parentUser", userObject);
-        babyQuery.first().then( function( babyObject ){
-            // Check if baby exists and add it to the email body.
-            // Add user email / baby name to the subject if baby exists.
-            if ( babyObject){
-                var name = babyObject.get("name");
-                subject = subject + "/" + name;
-                notificationObject.baby = babyObject;
-            }
-            // Send morgan an email!
-            require("cloud/teamnotify").notify(subject, notificationObject).then(function () {
-                response.success();
-            })
-
-        }, function(error) {
-            console.warn("There was an error on the babyQuery first method while saving User");
-        });
-    } else {
-        response.success();
+    if (userObject.dirty("email")) {
+        if(userObject.get("email")) {
+            // Wait until assignment is done.
+            userObject.set("needsTipAssignmentNow", true);
+        } else {
+            response.error("Can not set the already extant email '' to null for " +
+                    userObject.id ? ("user id " + userObject.id) : ("username " + userObject.get("username")));
+            return;
+        }
     }
+    response.success();
 });
 
 Parse.Cloud.afterSave(Parse.User, function (request) {
     var userObject = request.object;
     if (userObject.get("needsTipAssignmentNow")) {
+        userObject.set("needsTipAssignmentNow", false);
+        userObject.save();
+
         var babyQuery = new Parse.Query("Babies");
         babyQuery.equalTo("parentUser", userObject);
         var tips = require("cloud/tips");
         tips.processBabies(babyQuery, true).then(function () {
-            if (arguments.length > 0) {
-                userObject.set("needsTipAssignmentNow", false);
-                userObject.save();
-            } else {
+            if (arguments.length == 0) {
              // Else, no babies yet, we'll do this when a baby is saved.
              console.warn("No tip assignments made because no babies found for user " + userObject.id);
             }
+        });
 
+        var notifier = require("cloud/teamnotify");
+        var userName = userObject.get('email');
+        var subject = "A user has signed up: " + userName;
+        var notificationObject = { user : userObject};
+        babyQuery.first().then( function( babyObject ){
+            // Check if baby exists and add it to the email body.
+            // Add user email / baby name to the subject if baby exists.
+            if (babyObject){
+                var name = babyObject.get("name");
+                subject = subject + " with baby '" + name + "'";
+                notificationObject.baby = babyObject;
+            }
+            // Send morgan an email!
+            notifier.notify(subject, notificationObject);
+        }, function(error) {
+            console.warn("There was an error looking up the baby in afterSave: " + JSON.stringify(error));
+            notifier.notify(subject, notificationObject);
         });
     }
 });
 
-Parse.Cloud.beforeSave(Parse.Installation, function (request, response) {
-    if (!request.object.id) {
-        require("cloud/teamnotify").notify("Someone just installed the app!", request.object).then(function () {
-            response.success();
-        });
-    } else {
-        response.success();
+Parse.Cloud.afterSave(Parse.Installation, function (request) {
+    if (!request.object.existed()) {
+        require("cloud/teamnotify").notify("Someone just installed the app!", request.object);
     }
 });
 
